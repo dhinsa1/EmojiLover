5c548d27b07d76de580095000bb20a245a7b1143
Dhinsa1 <ignore@ignore.com>
2024-04-04 15:58:27 -0400

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Vikram Dhinsa on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
